384b78312f7279d7e9135f35084f5461e0b93210
sanjayreddygone01-commits <sanjayreddygone01@email.com>
2026-02-12 18:33:22 +0530

    project created

---
+++ src/main/java/com/quickcommerce/thiskostha/ThiskosthaApplication.java
@@ -0,0 +1,13 @@
+package com.quickcommerce.thiskostha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiskosthaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiskosthaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickcommerce/thiskostha/ThiskosthaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickcommerce.thiskostha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiskosthaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
