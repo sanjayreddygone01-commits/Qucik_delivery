7eac1622d35643ffcb5ae231cbad405c1670e1c1
sanjayreddygone01-commits <sanjayreddygone01@email.com>
2026-02-14 18:47:54 +0530

    added properties and responsestructure

--- src/main/java/com/quickcommerce/thiskostha/controller/CustomerController.java
+++ src/main/java/com/quickcommerce/thiskostha/controller/CustomerController.java
@@ -1,5 +1,26 @@
 package com.quickcommerce.thiskostha.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quickcommerce.thiskostha.dto.CustomerDTO;
+import com.quickcommerce.thiskostha.dto.ResponseStructure;
+import com.quickcommerce.thiskostha.entity.Customer;
+import com.quickcommerce.thiskostha.service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+	@Autowired
+	private CustomerService customerService;
+	@PostMapping("/register")
+	public ResponseEntity<ResponseStructure<Customer>> register(@RequestBody CustomerDTO customerdto){
+		return customerService.register(customerdto);
+		
+	}
 
 }
--- src/main/java/com/quickcommerce/thiskostha/service/CustomerService.java
+++ src/main/java/com/quickcommerce/thiskostha/service/CustomerService.java
@@ -1,5 +1,38 @@
 package com.quickcommerce.thiskostha.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.quickcommerce.thiskostha.dto.CustomerDTO;
+import com.quickcommerce.thiskostha.dto.ResponseStructure;
+import com.quickcommerce.thiskostha.entity.Customer;
+import com.quickcommerce.thiskostha.repository.CustomerRepository;
+
+@Service
 public class CustomerService {
+	@Autowired
+	private CustomerRepository customerRepo;
+
+	public ResponseEntity<ResponseStructure<Customer>> register(CustomerDTO customerdto) {
+		Customer customer =new Customer();
+		customer.setName(customerdto.getName());
+		customer.setPhone(customerdto.getPhone());
+		customer.setEmail(customerdto.getEmail());
+		customer.setGender(customerdto.getGender());
+		
+		customerRepo.save(customer);
+		ResponseStructure<Customer> rs = new ResponseStructure<Customer>();
+		rs.setStatuscode(HttpStatus.CREATED.value());
+		rs.setMessage("customer saved successfully");
+		rs.setData(customer);
+		
+		return new ResponseEntity<ResponseStructure<Customer>>(rs,HttpStatus.CREATED);
+		
+	}
+	
+	
+	
 
 }
