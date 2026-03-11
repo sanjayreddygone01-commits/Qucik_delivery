262104dbe4554532b6c3a8da1322a19889b1d5b3
sanjayreddy1902 <sanjayreddygone01@gmail.com>
2026-03-11 16:32:04 +0530

    Add CancelOrderDTO class for cancel order request

---
+++ src/main/java/com/quickcommerce/thiskostha/dto/CancelOrderDTO.java
@@ -0,0 +1,35 @@
+public class CancelOrderDTO {
+    private String orderId;
+    private String phone;
+    private String cancellationReason;
+
+    public CancelOrderDTO(String orderId, String phone, String cancellationReason) {
+        this.orderId = orderId;
+        this.phone = phone;
+        this.cancellationReason = cancellationReason;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCancellationReason() {
+        return cancellationReason;
+    }
+
+    public void setCancellationReason(String cancellationReason) {
+        this.cancellationReason = cancellationReason;
+    }
+}
